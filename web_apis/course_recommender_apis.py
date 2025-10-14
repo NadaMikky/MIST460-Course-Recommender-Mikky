0024b8333d581c298731d388e36ad06b5fe2253a
Nada Mikky <142957229+NadaMikky@users.noreply.github.com>
2025-10-14 16:25:41 -0400

    first commit

---
+++ web_apis/course_recommender_apis.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import pyodbc
+
+app = FastAPI()
+
+def main ():
+    import uvicorn
+   uvcorn.run(app, host="0.0.0.0", port=8000)
+   if __name__ == "__main__":
+    main()
+
+    # Databse connection parameters 
+    DB_SERVER = 'localhost'
+    DB_DATABASE = 'Homework3Group1'
+    DB_USERNAME = 'your_username'
+    DB_PASSWORD = 'your_password'
+    DB_DRIVER = '{ODBC Driver 17 for SQL Server}'
+
+    def get_db_connection():
+
+        try:
+        conn_str = 
+        f'DRIVER={DB_DRIVER}; SERVER={DB_SERVER};DATABASE={DB_DATABASE}; 
+        trusted_connection=yes;'
+
+        return pyodbc.connect(conn_str)
+
+        except Exception as e:
+            print(f"ERROR connecting to database: {e}")
+            raise HTTPException(status_code=500, detail="Database connection error")
+
+            @app.get("/find_current_semester_course_offerings")
+            def find_current_semester_course_offerings(
+                subject_code: str,
+                course_number: str
+                ):
+                conn = get_db_connection()
+
+                cursor = conn.cursor()  
+                cursor.execute("{CALL procFindCurrentSemesterCourseOfferingsForSpecifiedCourse(?)}",
+                 subject_code, course_number)
+                 rows = cursor.fetchall()
+                cursor.close()
+                conn.close()
+                #convert rows to list of dictionaries for better JSON serialization
+                results = [
+                    {
+                        "SubjectCode": row.SubjectCode,
+                        "CourseNumber": row.CourseNumber,
+                        "CRN": row.CRN,
+                        "Semester": row.Semester,
+                        "Year": row.Year,
+                        "CourseOfferingID": row.CourseOfferingID,
+                        "NumberSeatsRemaining": row.NumberSeatsRemaining
+                    }
+                    for row in rows
+
+                ]
+                return {"data": results}
+                
